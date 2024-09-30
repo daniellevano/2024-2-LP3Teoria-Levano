83f90e73d852418c46ae6a74e414add74afb18d9
Daniel Levano <levanod@gmail.com>
2024-09-30 15:21:51 -0500

    Semana06 - crear archivos txt

---
+++ Semana06/grabar_archivo.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 15:17:02 2024
+
+@author: Daniel Levano
+"""
+
+archivo= open("archivo_de_prueba.txt","wt")
+contenido = "Saludos cordiales\nAtentamente\nDaniel"
+archivo.write(contenido)
+archivo.close()
+
