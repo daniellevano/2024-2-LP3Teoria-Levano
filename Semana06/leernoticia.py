cde79780fada2f730da0ba7dec8ddd8e79e76490
Daniel Levano <levanod@gmail.com>
2024-09-30 15:16:50 -0500

    Semana06 - Leer archivos con caracteres especiales

--- Semana06/leernoticia.py
+++ Semana06/leernoticia.py
@@ -5,6 +5,6 @@ Created on Mon Sep 30 15:08:40 2024
 @author: Daniel Levano
 """
 
-noticia = open("noticia.txt","rt")
+noticia = open("noticia.txt","rt", encoding='utf8')
 datos_noticia = noticia.read()
 print(datos_noticia)
