bc3f5540caa41272c6b8cc443d33bf1d0c5a1f0b
Daniel Levano <levanod@gmail.com>
2024-09-30 15:13:29 -0500

    Semana06 - Leer archivos

---
+++ Semana06/leernoticia.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 15:08:40 2024
+
+@author: Daniel Levano
+"""
+
+noticia = open("noticia.txt","rt")
+datos_noticia = noticia.read()
+print(datos_noticia)
